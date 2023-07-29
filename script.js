8609392410a4fc376df5f44120090ed9ef20c3be
Beuuuh <bellmarques8768@hotmail.com>
2023-07-29 19:08:52 -0300

    mark the board funcion

--- script.js
+++ script.js
@@ -1,10 +1,23 @@
-const gameBoard = () => {
+const gameBoard = (() => {
     const DIVBOARD = document.querySelector(".gameboard");
+    
     const createBoard = () => {
         for(i = 0; i < 9; i++) {
             let div = document.createElement("div");
             div.setAttribute("class", "pixel");
             DIVBOARD.appendChild(div);
+            div.addEventListener("click", () => {
+                markTheBoard(div);
+            });
         };
     };
-};
+
+    const markTheBoard = (div) => {
+        div.innerText = "X";
+    }; 
+
+    return {
+        createBoard,
+        markTheBoard
+    };
+})();
