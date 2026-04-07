a925a54f30f1dce6d1ac60e8bf5e9e07d916d958
Beuuuh <belldiscord8768@hotmail.com>
2026-04-07 00:14:39 -0300

    feat: adds new functionalities

--- script.js
+++ script.js
@@ -1,13 +1,29 @@
 const gameBoard = (() => {
-    const DIVBOARD = document.querySelector(".gameboard");
+    const divBoard = document.querySelector(".gameboard");
+    const label = document.querySelector(".label");
+    const turn = document.querySelector(".turn");
+
+    let playerTurn = true;
+    let playerScore = 0;
+    let computerScore = 0;
     
     const createBoard = () => {
-        for(i = 0; i < 9; i++) {
+        for(let i = 0; i < 9; i++) {
             let div = document.createElement("div");
             div.setAttribute("class", "pixel");
-            DIVBOARD.appendChild(div);
+
+            div.style.width = "200px";
+            div.style.height = "200px";
+            div.style.backgroundColor = "cyan";
+            div.style.border = "1px solid black";
+            div.style.borderColor = "black";
+
+            divBoard.appendChild(div);
+
             div.addEventListener("click", () => {
                 markTheBoard(div);
+                showTurn(playerTurn ? "Player" : "Computer");
+                playerTurn = !playerTurn;
             })
         }
     }
@@ -16,8 +32,24 @@ const gameBoard = (() => {
         div.innerText = "X";
     }
 
+    const startGame = () => {
+        showScore(playerScore, computerScore);
+        createBoard();
+        showTurn("Player");
+        playerTurn = !playerTurn;
+    }
+
+    const showScore = (playerScore, computerScore) => {
+        label.innerText = `Player: ${playerScore} vs Computer: ${computerScore}`;
+    }
+
+    const showTurn = (somebodyTurn) => {
+        turn.innerText = `It's ${somebodyTurn}'s turn`;
+    }
+
     return {
-        createBoard,
-        markTheBoard
+        startGame
     }
 })()
+
+gameBoard.startGame();
